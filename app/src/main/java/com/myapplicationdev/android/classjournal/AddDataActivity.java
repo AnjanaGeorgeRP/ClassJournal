e45ffaed60a46dcabf76450882389c854f073ce3
AnjanaGeorge <16004097@myrp.edu.sg>
2018-05-07 13:00:37 +0800

    commit and push

--- app/src/main/java/com/myapplicationdev/android/classjournal/AddDataActivity.java
+++ app/src/main/java/com/myapplicationdev/android/classjournal/AddDataActivity.java
@@ -1,14 +1,33 @@
 package com.myapplicationdev.android.classjournal;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class AddDataActivity extends AppCompatActivity {
 
-
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_data);
+
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rg = (RadioGroup) findViewById(R.id.rgGrade);
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+                String grade = rb.getText().toString();
+                
+            }
+        });
+
     }
 }
