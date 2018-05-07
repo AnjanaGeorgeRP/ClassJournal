054f3518978dd813d65fea6b97d4eab9d6413931
shawnwong65 <16022774@rp.edu.sg>
2018-05-07 13:17:45 +0800

    Update

--- app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
@@ -3,9 +3,17 @@ package com.myapplicationdev.android.classjournal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class SecondActivity extends AppCompatActivity {
+
+    ListView lvGrades;
+    ArrayAdapter aa;
+    ArrayList<DailyCA> dailyCA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,13 @@ public class SecondActivity extends AppCompatActivity {
         Module module = (Module) intent.getSerializableExtra("module");
         getActionBar().setTitle("Info for " + module.getModuleCode());
         getSupportActionBar().setTitle("Info for " + module.getModuleCode());
+
+        lvGrades = (ListView) findViewById(R.id.lvGrades);
+
+        dailyCA = new ArrayList<DailyCA>();
+        dailyCA.add(new DailyCA("B", module.getModuleCode(), 1));
+
+        aa = new DailyCAArrayAdapter(this, R.layout.row, dailyCA);
+        lvGrades.setAdapter(aa);
     }
 }
