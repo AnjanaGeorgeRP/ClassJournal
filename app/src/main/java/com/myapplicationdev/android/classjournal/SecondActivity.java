12d296f50dd6b707c020c3aca44c3c810615c639
shawnwong65 <16022774@rp.edu.sg>
2018-05-07 13:33:43 +0800

    Update

--- app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
@@ -3,7 +3,9 @@ package com.myapplicationdev.android.classjournal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,6 +16,10 @@ public class SecondActivity extends AppCompatActivity {
     ListView lvGrades;
     ArrayAdapter aa;
     ArrayList<DailyCA> dailyCA;
+    Button btnInfo, btnAdd, btnEmail;
+    int requestCodeForInfo = 1;
+    int requestCodeForAdd = 2;
+    int requestCodeForEmail = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +38,57 @@ public class SecondActivity extends AppCompatActivity {
 
         aa = new DailyCAArrayAdapter(this, R.layout.row, dailyCA);
         lvGrades.setAdapter(aa);
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SecondActivity.this, AddDataActivity.class);
+                startActivityForResult(i, requestCodeForAdd);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Only handle when 2nd activity closed normally
+        //  and data contains something
+        if(resultCode == RESULT_OK){
+            if (data != null) {
+                // Get data passed back from 2nd activity
+
+                // If it is activity started by clicking 				//  Superman, create corresponding String
+                if(requestCode == requestCodeForInfo){
+
+                }
+                // If 2nd activity started by clicking
+                //  Batman, create a corresponding String
+                if(requestCode == requestCodeForAdd){
+
+                }
+
+                if(requestCode == requestCodeForEmail){
+
+                }
+            }
+        }
+    }
+
 }
