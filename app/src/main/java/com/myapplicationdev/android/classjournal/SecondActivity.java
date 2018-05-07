dabead21755a6d3784bcdc8e04109ce54a135b6d
shawnwong65 <16022774@rp.edu.sg>
2018-05-07 13:34:50 +0800

    Update

--- app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
@@ -1,7 +1,6 @@
 package com.myapplicationdev.android.classjournal;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,24 +12,25 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
-    Button btnInfo, btnAdd, btnEmail;
+
     ListView lvGrades;
     ArrayAdapter aa;
     ArrayList<DailyCA> dailyCA;
+    Button btnInfo, btnAdd, btnEmail;
+    int requestCodeForInfo = 1;
+    int requestCodeForAdd = 2;
+    int requestCodeForEmail = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        btnInfo = findViewById(R.id.buttonInfo);
-        btnAdd = findViewById(R.id.buttonAdd);
-        btnEmail = findViewById(R.id.buttonEmail);
-
         Intent intent = getIntent();
-        final Module module = (Module) intent.getSerializableExtra("module");
+        Module module = (Module) intent.getSerializableExtra("module");
         getSupportActionBar().setTitle("Info for " + module.getModuleCode());
         getSupportActionBar().setTitle("Info for " + module.getModuleCode());
+
         lvGrades = (ListView) findViewById(R.id.lvGrades);
 
         dailyCA = new ArrayList<DailyCA>();
@@ -38,13 +38,57 @@ public class SecondActivity extends AppCompatActivity {
 
         aa = new DailyCAArrayAdapter(this, R.layout.row, dailyCA);
         lvGrades.setAdapter(aa);
+
         btnInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent moduleIntent = new Intent(Intent.ACTION_VIEW);
-                moduleIntent.setData(Uri.parse(module.getModuleLink()));
-                startActivity(moduleIntent);
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SecondActivity.this, AddDataActivity.class);
+                startActivityForResult(i, requestCodeForAdd);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+
+
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Only handle when 2nd activity closed normally
+        //  and data contains something
+        if(resultCode == RESULT_OK){
+            if (data != null) {
+                // Get data passed back from 2nd activity
+
+                // If it is activity started by clicking 				//  Superman, create corresponding String
+                if(requestCode == requestCodeForInfo){
+
+                }
+                // If 2nd activity started by clicking
+                //  Batman, create a corresponding String
+                if(requestCode == requestCodeForAdd){
+
+                }
+
+                if(requestCode == requestCodeForEmail){
+
+                }
+            }
+        }
+    }
+
 }
