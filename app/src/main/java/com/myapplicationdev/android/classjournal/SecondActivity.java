02fd4ae2e77243cfb629e63ee7144478e1b25b21
liang3336 <632944695@qq.com>
2018-05-07 13:18:56 +0800

    yaoliang commit6 - add Function for btnInfo

--- app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/classjournal/SecondActivity.java
@@ -1,20 +1,38 @@
 package com.myapplicationdev.android.classjournal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button  btnInfo, btnAdd, btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        btnInfo = findViewById(R.id.buttonInfo);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnEmail = findViewById(R.id.buttonEmail);
+
         Intent intent = getIntent();
-        Module module = (Module) intent.getSerializableExtra("module");
+        final Module module = (Module) intent.getSerializableExtra("module");
         getSupportActionBar().setTitle("Info for " + module.getModuleCode());
         getSupportActionBar().setTitle("Info for " + module.getModuleCode());
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent moduleIntent = new Intent(Intent.ACTION_VIEW);
+                moduleIntent.setData(Uri.parse(module.getModuleLink()));
+                startActivity(moduleIntent);
+            }
+        });
     }
 }
